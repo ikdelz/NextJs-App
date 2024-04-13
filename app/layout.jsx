3479bf7553b31e8b33ab039adb4f6c648909e18a
Elise <ielyse119@gmail.com>
2024-04-13 21:05:23 +0200

    nextjs app

---
+++ app/layout.jsx
@@ -0,0 +1,17 @@
+import { Quicksand } from "next/font/google";
+import "./globals.css";
+
+const quicksand = Quicksand({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "NextJs App - Elise",
+  description: "An app created with NextJs",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={quicksand.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="app">
+      <h1 className="h1">Hello ma people</h1>
+    </div>
+  );
+}
